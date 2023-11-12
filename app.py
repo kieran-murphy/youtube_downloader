07a7c9d88600203a9dd4e32bd81b4d0e42f65072
kieran-murphy <68723269+kieran-murphy@users.noreply.github.com>
2023-11-13 01:16:51 +1000

    Added download video option

--- app.py
+++ app.py
@@ -41,9 +41,50 @@ def download_audio(url, path):
     os.remove(path + "temp.mp4")
 
 
-def log_text():
+def download_video(url, path):
+    yt = YouTube(url)
+
+    # Get the highest quality video-only stream
+    video_stream = (
+        yt.streams.filter(adaptive=True, file_extension="mp4", only_video=True)
+        .order_by("resolution")
+        .desc()
+        .first()
+    )
+
+    # Get the highest quality audio stream
+    audio_stream = yt.streams.filter(only_audio=True).order_by("abr").desc().first()
+
+    # Download video and audio streams
+    video_filename = video_stream.download(output_path=path, filename_prefix="video_")
+    audio_filename = audio_stream.download(output_path=path, filename_prefix="audio_")
+
+    # Load the downloaded video and audio
+    video_clip = VideoFileClip(video_filename)
+    audio_clip = AudioFileClip(audio_filename)
+
+    # Set the audio of the video clip
+    final_clip = video_clip.set_audio(audio_clip)
+
+    # Write the final clip
+    final_clip.write_videofile(path + yt.title + ".mp4")
+
+    # Remove the temporary files
+    os.remove(video_filename)
+    os.remove(audio_filename)
+
+
+def download_audio_action():
+    sys.stdout = open(os.devnull, "w")
     text = text_input.text()
     download_audio(text, "downloads/")
+    text_input.clear()  # Clear the text input
+    sys.stdout = sys.__stdout__
+
+
+def download_video_action():
+    text = text_input.text()
+    download_video(text, "downloads/")
     text_input.clear()  # Clear the text input
 
 
@@ -69,17 +110,24 @@ text_input.setFont(font)
 layout.addWidget(text_input)
 
 # Create a button with a different color and styling
-log_button = QPushButton("Download")
+log_button = QPushButton("Download Audio")
 font = QFont()
 font.setPointSize(12)
 log_button.setFont(font)
 log_button.setStyleSheet("background-color: green;")
-log_button.clicked.connect(log_text)
+log_button.clicked.connect(download_audio_action)
 layout.addWidget(log_button)
+
+download_video_button = QPushButton("Download Video")
+download_video_button.setFont(font)
+download_video_button.setStyleSheet("background-color: blue;")
+download_video_button.clicked.connect(download_video_action)
+layout.addWidget(download_video_button)
 
 central_widget.setLayout(layout)
 
 window.resize(400, 200)
 window.show()
+
 
 sys.exit(app.exec_())
